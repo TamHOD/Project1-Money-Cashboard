d8921b0665f6329999e4e24dbe870411f7414f4f
Alex Bazlinton <abazlinton@gmail.com>
2016-10-22 18:44:14 +0100

    Added some css and more detail on views

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require '../models/tag'
 
 Tag.delete_all
 
-@tag1 = Tag.new('name' => 'food', 'color' => 'green', 'description' => 'Total food budget', 'budget' => 200)
+@tag1 = Tag.new('name' => 'food', 'color' => 'lightblue', 'description' => 'Total food budget', 'budget' => 200)
 @tag1.save
 
 
