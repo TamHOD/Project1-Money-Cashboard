a345c7bd41a08b5ea41dc510b100aa5f8bd980e2
Alex Bazlinton <abazlinton@gmail.com>
2016-10-28 22:08:57 +0100

    now using ENV for login / server

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -5,7 +5,6 @@ class SqlRunner
   def self.run( sql )
 
     uri = URI.parse(ENV['DATABASE_URL'])
-    puts uri
 
     begin
       db = PG.connect( dbname: uri.path[1..-1], host: uri.host, user: uri.user, password: uri.password)
