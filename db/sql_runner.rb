33156e1a966956bac9b59c39b519720cde76c786
Alex Bazlinton <abazlinton@gmail.com>
2016-10-28 22:41:03 +0100

    set to localhost if ENV not set

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,8 +4,12 @@ class SqlRunner
 
   def self.run( sql )
 
-    uri = URI.parse(ENV['DATABASE_URL'])
-
+    if ENV['DATABASE_URL'] == nil
+      uri = URI.parse("http://localhost/money_cashboard")
+    else
+      uri = URI.parse(ENV['DATABASE_URL'])    
+    end
+    
     begin
       db = PG.connect( dbname: uri.path[1..-1], host: uri.host, user: uri.user, password: uri.password)
       result = db.exec ( sql )
