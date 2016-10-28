9ed57fc1f5382d1ae9f0112bd62f0571761e97eb
Alex Bazlinton <abazlinton@gmail.com>
2016-10-28 21:56:47 +0100

    db settings

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,7 +1,11 @@
 require 'pg'
+require 'uri'
 class SqlRunner
 
   def self.run( sql )
+
+    uri = URI.parse(ENV['DATABASE_URL'])
+    puts uri
 
     begin
       db = PG.connect( dbname: 'd8q2cn0mhs8jll', host: 'ec2-54-228-214-47.eu-west-1.compute.amazonaws.com', user: 'bupxdrnxajhdvt', password: 'MGwsRC9Z9FqP9QiDQmcxbUQBQb')
