30ef207f91b70c5d3e1b7416981bf478f9bed1e2
Alex Bazlinton <abazlinton@gmail.com>
2016-10-28 20:08:04 +0100

    db settings

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
   def self.run( sql )
 
     begin
-      db = PG.connect( dbname: 'money_cashboard', host: 'localhost')
+      db = PG.connect( dbname: 'd8q2cn0mhs8jll', host: 'ec2-54-228-214-47.eu-west-1.compute.amazonaws.com')
       result = db.exec ( sql )
 
     ensure
